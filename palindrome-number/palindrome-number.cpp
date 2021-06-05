9e2de22a0eb7889010e4bdbf1e1bcbcc268f6c89
Yogendra07 <51382494+Yogendra07@users.noreply.github.com>
2021-06-06 00:57:04 +0530

    Time: 12 ms (49.72%), Space: 5.9 MB (67.74%) - LeetHub

---
+++ palindrome-number/palindrome-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        if(x < 0)
+        {
+            return false;
+        }
+        else
+        {
+            string s = to_string(x);
+            
+            int i = 0;
+            int j = s.length() - 1;
+            
+            while(i<j)
+            {
+                if(s[i] != s[j])
+                {
+                    return false;
+                }
+                
+                i++;
+                j--;
+            }
+            
+            return true;
+        }
+        
+        return false;
+    }
+};
