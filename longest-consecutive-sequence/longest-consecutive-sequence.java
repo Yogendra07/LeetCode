261b94722222918a1ac93731661ca8db1f0f1f90
Yogendra07 <51382494+Yogendra07@users.noreply.github.com>
2021-06-06 18:29:25 +0530

    Update longest-consecutive-sequence.java

--- longest-consecutive-sequence/longest-consecutive-sequence.java
+++ longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,4 +1,5 @@
-class Solution {
+class Solution 
+{
     public int longestConsecutive(int[] nums) 
     {
         if(nums.length == 0)
@@ -6,28 +7,33 @@ class Solution {
             return 0;
         }
         
-        Arrays.sort(nums);    
+        Set<Integer> s = new HashSet<Integer>();
         
-        int current_streak = 1;
-        int longest_streak = 1;
-        
-        for(int i=1;i<nums.length;i++)
+        for(int x : nums)
         {
-            if(nums[i] != nums[i-1])
-            {
-                if(nums[i] == nums[i-1] + 1)
-                {
-                    current_streak +=1;
-                }
-                else
-                {
-                    longest_streak = Math.max(longest_streak,current_streak);
-                    current_streak  = 1;
-                }
-            }
+            s.add(x);
         }
         
-        return Math.max(current_streak,longest_streak);
+         int max_len = 0;
+        
+        for(int num : s)
+        {
+            if(s.contains(num-1))
+            {
+                continue;
+            }
+            
+            int len = 1;
+            
+            while(s.contains(num + len))
+            {
+                len++;
+            }
+            
+            max_len = Math.max(max_len,len);
+            
+        }
+        
+        return max_len;
     }
-    
 }
