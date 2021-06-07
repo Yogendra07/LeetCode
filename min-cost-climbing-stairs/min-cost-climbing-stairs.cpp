d3a09c72265cd69421d6a4008412c998cc57919a
Yogendra07 <51382494+Yogendra07@users.noreply.github.com>
2021-06-07 13:34:06 +0530

    Update min-cost-climbing-stairs.cpp

--- min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& nums) 
+    int minCostClimbingStairs(vector<int>& cost) 
     {
-        int dp[1002] = {0};
+        int dp[10002] = {0};
         
-        if(nums.size() <= 2)
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2;i<cost.size();i++)
         {
-            dp[0] = nums[0];
-            dp[1] = min(nums[0],nums[1]);
+            dp[i] = cost[i] + min(dp[i-1],dp[i-2]);
         }
-        else
-        {
-            dp[0] = nums[0] , dp[1] = nums[1];
-            
-            for(int i=2;i<nums.size();i++)
-            {
-                if(i != nums.size()-1)
-                {
-                    dp[i] = nums[i] + min(dp[i-1],dp[i-2]);
-                }
-                else
-                {
-                    int ans1 = dp[i-1];
-                    int ans2 = nums[i] + dp[i-2];
-                    dp[i] = min(ans1,ans2);
-                }
-            }
-        }
-    
-        return dp[nums.size()-1];
+        
+        return min(dp[cost.size()-1],dp[cost.size()-2]);
     }
 };
